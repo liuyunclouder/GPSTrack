f5b607be18644c58171bbd33a1a13fa1b9957888
Openthread <openfibers@gmail.com>
2012-10-21 21:45:43 +0800

    Initial Commit

---
+++ GPSTrack/OTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OTAppDelegate.h
+//  GPSTrack
+//
+//  Created by OpenThread on 10/21/12.
+//  Copyright (c) 2012 OpenThread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GPSTrack/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GPSTrack
+//
+//  Created by OpenThread on 10/21/12.
+//  Copyright (c) 2012 OpenThread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OTAppDelegate class]));
+    }
+}
